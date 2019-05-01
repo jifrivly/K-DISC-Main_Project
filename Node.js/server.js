140d43f5da4a41b03fcb77be8e5b13151b381fd7
jifrivly <jifrivly@gmail.com>
2019-05-01 16:49:54 +0530

    Nodejs backend working

--- Node.js/server.js
+++ Node.js/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const chalk = require("chalk");
 
 my_app = express();
 
+my_app.get("/",(req,res)=>{
+	res.send("Ya Working");
+});
+
 my_app.listen("4545",()=>{
-	console.log("Backend is working");
+	console.log(chalk.red("Backend is working"));
 });
